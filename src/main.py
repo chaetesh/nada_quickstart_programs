f87296d62393d9177642f6683b6b6a7e5cc577a8
chaetesh <MY_NAME@example.com>
2024-07-20 12:15:43 +0530

    done

--- src/main.py
+++ src/main.py
@@ -1,20 +1,12 @@
 from nada_dsl import *
 
-
 def nada_main():
-    # Define parties (bidders)
-    bidder1 = Party(name="Bidder1")
-    bidder2 = Party(name="Bidder2")
-    bidder3 = Party(name="Bidder3")
+    party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
+    party3 = Party(name="Party3")
+    a = SecretInteger(Input(name="A", party=party1))
+    b = SecretInteger(Input(name="B", party=party2))
 
-    # Define secret bid inputs
-    bid1 = SecretInteger(Input(name="bid1", party=bidder1))
-    bid2 = SecretInteger(Input(name="bid2", party=bidder2))
-    bid3 = SecretInteger(Input(name="bid3", party=bidder3))
+    result = a + b
 
-    # Compute the highest bid
-    highest_bid = max(bid1, max(bid2, bid3))
-
-    # Output the highest bid to all participants
-    return [Output(highest_bid, "highest_bid", [bidder1, bidder2, bidder3])]
-
+    return [Output(result, "my_output", party3)]
--- src/secret_addition.py
+++ src/secret_addition.py
@@ -2,18 +2,18 @@ from nada_dsl import *
 
 
 def nada_main():
-    # Define parties
-    employee1 = Party(name="Employee1")
-    employee2 = Party(name="Employee2")
-    employee3 = Party(name="Employee3")
+    # Define parties (bidders)
+    bidder1 = Party(name="Bidder1")
+    bidder2 = Party(name="Bidder2")
+    bidder3 = Party(name="Bidder3")
 
-    # Define secret salary inputs
-    salary1 = SecretInteger(Input(name="salary1", party=employee1))
-    salary2 = SecretInteger(Input(name="salary2", party=employee2))
-    salary3 = SecretInteger(Input(name="salary3", party=employee3))
+    # Define secret bid inputs
+    bid1 = SecretInteger(Input(name="bid1", party=bidder1))
+    bid2 = SecretInteger(Input(name="bid2", party=bidder2))
+    bid3 = SecretInteger(Input(name="bid3", party=bidder3))
 
-    # Compute the total salary
-    total_salary = salary1 + salary2 + salary3
+    # Compute the highest bid
+    highest_bid = max(bid1, max(bid2, bid3))
 
-    # Output the average salary to all employees
-    return [Output(total_salary, "average_salary", [employee1, employee2, employee3])]
+    # Output the highest bid to all participants
+    return [Output(highest_bid, "highest_bid", [bidder1, bidder2, bidder3])]
